440c5e6538e7186c5a4f357508b6a6bbb0fb6824
Abebe Duguma <abebeduguma27@gmail.com>
2025-07-06 13:22:55 +0300

    Apply Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,29 @@
+-- Total Number of Bookings Per User (Using COUNT + GROUP BY)
+
+SELECT
+    U.user_id,
+    U.first_name,
+    U.last_name,
+    COUNT(B.booking_id) AS total_bookings
+FROM User AS U
+LEFT JOIN Booking AS B ON U.user_id = B.user_id
+GROUP BY
+    U.user_id, U.first_name, U.last_name
+ORDER BY
+    total_bookings DESC;
+
+  -- Rank Properties by Total Bookings (Using RANK() Window Function)
+
+  SELECT
+    P.property_id,
+    P.name AS property_name,
+    COUNT(B.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(B.booking_id) DESC) AS booking_rank
+FROM
+    Property AS P
+LEFT JOIN
+    Booking AS B ON P.property_id = B.property_id
+GROUP BY
+    P.property_id, P.name
+ORDER BY
+    booking_rank;
